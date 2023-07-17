3a3309b5d30ebd5e8cf1aec9dc30d2c6d2e5dc70
admin <admin@LAPTOP-RB3SNKG0>
2023-07-17 16:25:01 +0700

    Add project files.

---
+++ W3b_Lap/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace W3b_Lap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
